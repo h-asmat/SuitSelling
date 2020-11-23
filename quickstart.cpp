c70b289a1acdac5b4f6bd726c3bb9a35f33bdd02
Hilal Asmat <hilalasmat@hotmail.ca>
2020-11-22 19:18:40 -0800

    Add files via upload

--- quickstart.cpp
+++ quickstart.cpp
@@ -1,18 +1,8 @@
  #include "simdjson.h"
  #include <string>
- #include <stdio.h>
- #include <unistd.h>
-#include <stdlib.h>
-#define INPUTSIZE 10000000
-
    int main(int argc, char *argv[]) {
      simdjson::dom::parser parser;
-     char input[INPUTSIZE] = {0};
-
-     if (read(STDIN_FILENO, input, INPUTSIZE) < 0)
-	{
-		fprintf(stderr, "Couldn't read stdin.\n");
-	}
-     simdjson::dom::element json = parser.load(input);
+     std::string path(argv[1]);
+     simdjson::dom::element json = parser.load(path);
      std::cout << json << std::endl;
    }
