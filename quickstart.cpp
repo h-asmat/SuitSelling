d68b8aaf8106252445a129e18664013ae10d2bc6
Hilal Asmat <hilalasmat@hotmail.ca>
2020-11-22 19:40:59 -0800

    Add files via upload

--- quickstart.cpp
+++ quickstart.cpp
@@ -3,6 +3,12 @@
    int main(int argc, char *argv[]) {
      simdjson::dom::parser parser;
      std::string path(argv[1]);
-     simdjson::dom::element json = parser.load(path);
-     std::cout << json << std::endl;
+     try {
+        simdjson::dom::element json = parser.load(path);
+        std::cout << json << std::endl;
+     }
+     catch (simdjson::simdjson_error err)
+     {
+         
+     }
    }
